d089c1a567887bf13ad109fb61d315c5787ce331
lucasviotti94 <lucasviotti@iresm.edu.ar>
2022-12-05 16:31:39 -0300

    Delete useless text

--- api/index.js
+++ api/index.js
@@ -5,9 +5,8 @@ const {
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
-// Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(PORT || 3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log('%s listening at 3001');
   });
 });
