f277b2c4730d40604a9a006b86396c87497156e9
lucasviotti94 <lucasviotti@iresm.edu.ar>
2022-12-05 19:44:23 -0300

    Trying deleting some dependencies

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 const {
-  PORT,
+  PORT
 } = process.env;
 
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 conn.sync({ force: true }).then(() => {
-  server.listen(PORT, () => {
-    console.log(`%s listening at ${PORT}`);
+  server.listen(PORT || 3001, () => {
+    console.log('%s listening at 3001');
   });
 });
