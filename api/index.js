0f613ff41f2d666ae587d12fa054b011e831ee0b
lucasviotti94 <lucasviotti@iresm.edu.ar>
2022-12-05 17:14:22 -0300

    Nothin relevant

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 conn.sync({ force: true }).then(() => {
-  server.listen(PORT || 3001, () => {
-    console.log('%s listening at 3001');
+  server.listen(PORT, () => {
+    console.log(`%s listening at ${PORT}`);
   });
 });
